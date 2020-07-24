00981da166cca43e2b1cfe40ee6f412ddf897d0e
Umesh Badiger <42720103+umeshbadiger@users.noreply.github.com>
2020-07-24 12:59:51 +0530

    My First Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,50 @@
+import { RouterModule } from '@angular/router';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { AboutComponent } from './about/about.component';
+import { ExperienceComponent } from './experience/experience.component';
+import { EducationComponent } from './education/education.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { SkillsComponent } from './skills/skills.component';
+import { ContactComponent } from './contact/contact.component';
+import { HomeComponent } from './home/home.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSliderModule } from '@angular/material/slider';
+import {MatProgressBarModule} from '@angular/material/progress-bar';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    AboutComponent,
+    ExperienceComponent,
+    EducationComponent,
+    ProjectsComponent,
+    SkillsComponent,
+    ContactComponent,
+    HomeComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot([
+      { path: '', component: HomeComponent},
+      { path: 'about', component: AboutComponent},
+      { path: 'contact', component: ContactComponent},
+      { path: 'education', component: EducationComponent},
+      { path: 'experience', component: ExperienceComponent},
+      { path: 'portfolio', component: ProjectsComponent},
+      { path: 'skills', component: SkillsComponent}
+    ]),
+    BrowserAnimationsModule,
+    MatSliderModule,
+    MatProgressBarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
